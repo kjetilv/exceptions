d0e8f423ab24756b6a9af5016a26690db1fbb6ec
Kjetil Valstadsve <valstadsve@gmail.com>
2019-06-03 21:50:18 +0200

    Init commit

--- exceptions-server/build.gradle.kts
+++ exceptions-server/build.gradle.kts
@@ -19,9 +19,6 @@ dependencies {
     compile("org.http4k:http4k-server-netty:3.143.1")
     compile("org.http4k:http4k-format-jackson:3.143.1")
 
-    compile("org.http4k:http4k-client-apache:3.143.1")
-    compile("org.http4k:http4k-client-websocket:3.143.1")
-
     compile("io.swagger.core.v3:swagger-core:2.0.8")
     compile("io.micrometer:micrometer-registry-jmx:1.1.4")
 
@@ -31,5 +28,6 @@ dependencies {
     runtime("org.webjars:swagger-ui:3.22.2")
     runtime("ch.qos.logback:logback-classic:1.3.0-alpha4")
 
+    testCompile("org.http4k:http4k-client-apache:3.143.1")
     testCompile("junit:junit:4.12")
 }
